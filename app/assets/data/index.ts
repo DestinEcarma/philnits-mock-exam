f5f9e783a611e9d570610ca8c60bf6067f00ffd3
Dempsey <dagantareynat@gmail.com>
2025-02-01 08:41:56 +0800

    feat: added questions 31 - 60 from 2021A

--- app/assets/data/index.ts
+++ app/assets/data/index.ts
@@ -1,6 +1,7 @@
 import _2024 from '@/assets/data/2024/index.json'
 import _2023 from '@/assets/data/2023/index.json'
 import _2020 from '@/assets/data/2020/index.json'
+import _2021 from '@/assets/data/2021/index.json'
 
 export interface QuizQuestion {
   id: string
@@ -13,5 +14,6 @@ export interface QuizQuestion {
 export const quizData: QuizQuestion[] = [
   ..._2024,
   ..._2023,
+  ..._2021,
   ..._2020
 ]
