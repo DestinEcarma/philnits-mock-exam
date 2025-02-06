a04503ff0c178201f37ec0a715cee83933ca2f64
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-02-07 00:07:10 +0800

    feat: added 'contact if error is found' message

--- app/components/Credits.tsx
+++ app/components/Credits.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Credits = () => {
   return (
-    <p className="text-sm text-gray-500 flex gap-1">
-        Solutions and Explanations from 
-        <a target="_blank" href="https://github.com/usc-cisco/philnits-vault" className="underline">philnits-vault</a>
-    </p>
+    <div className='flex flex-col gap-2'>
+      <p className="text-sm text-gray-500 flex gap-1">
+          Solutions and Explanations from
+          <a target="_blank" href="https://github.com/usc-cisco/philnits-vault" className="underline">philnits-vault</a>
+      </p>
+      <p className='text-sm text-gray-500 flex gap-1'>
+        If you notice any errors, please message 
+        <a target="_blank" href="https://www.facebook.com/hai.its.me.zeus.da.kewl.guy.jk/" className="underline">Zeus Elderfield</a>
+      </p>
+    </div>
   )
 }
 
