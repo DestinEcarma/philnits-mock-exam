c6c75c29679832a8bd3d43c5bfe6a7a7a43322fc
elderfieldzeus <elderfieldzeus24@gmail.com>
2025-04-11 00:15:37 +0800

    fix: fixed weird phone layout for credits

--- app/components/Credits.tsx
+++ app/components/Credits.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Credits = () => {
   return (
     <div className='flex flex-col gap-2'>
-      <p className="text-sm text-gray-500 flex gap-1">
+      <p className="text-sm text-gray-500">
           Solutions and Explanations from
-          <a target="_blank" href="https://github.com/usc-cisco/philnits-vault" className="underline">philnits-vault</a>
+          <a target="_blank" href="https://github.com/usc-cisco/philnits-vault" className="underline ml-1">philnits-vault</a>
       </p>
-      <p className='text-sm text-gray-500 flex gap-1'>
+      <p className='text-sm text-gray-500'>
         If you notice any errors, please message 
-        <a target="_blank" href="https://www.facebook.com/hai.its.me.zeus.da.kewl.guy.jk/" className="underline">Zeus Elderfield</a>
+        <a target="_blank" href="https://www.facebook.com/elderfieldzeus/" className="underline ml-1">Zeus Elderfield</a>
       </p>
     </div>
   )
